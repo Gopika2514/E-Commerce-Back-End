bab899d25d60cdac66ed74cf5a60a4e52ecbb054
gopika <gopipatel2514@gmail.com>
2021-07-08 11:51:03 -0700

    little change in routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,7 +40,7 @@ router.get('/:id', async  (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create(req.id);
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -50,7 +50,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update({
+    const categoryData = await Category.update(req.body, {
       where: {
         id: req.params.id
       }
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,22 +26,13 @@ router.get('/', async (req, res) => {
   }
 });
 
-
-
-
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk({
-      where: { id: req.params.id },
-      include: [{
-        model: Category,
-        model: Tag,
-        through: ProductTag,
-
-      }]
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
     });
 
     res.status(200).json(productData);
